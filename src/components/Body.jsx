e0151dc5d561afef6a2e131c92f3a7cc89cc1aaf
ak2patel <ankit.2201116cs@iiitbh.ac.in>
2025-05-29 11:58:18 +0530

    created login

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './NavBar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import axios from 'axios'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from '../utils/userSlice'
 
 const Body = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const userData = useSelector((store)=> store.user);
+
+  const fetchUser = async()=>{
+  if(userData) return;
+    try{   
+    const res = await axios.get(BASE_URL+"/profile/view",{
+      withCredentials:true,
+    });
+    dispatch(addUser(res.data));
+  
+}catch(err){
+  if(err.response?.status==401){
+  navigate("/login")}
+  //  if (!userData || Object.keys(userData).length === 0) {
+  //     fetchUser()
+  //   }
+  console.error(err);
+}
+};
+ 
+  useEffect(()=>{
+    //  if(!userData){
+    //   fetchUser();
+    //    } 
+   fetchUser();
+
+  },[])
+
   return (
     <div className="flex flex-col min-h-screen">
         <Navbar/>
-        {/* <Outlet/> */}
         <main className="flex-grow"><Outlet /></main>
         <Footer/>
     </div>
