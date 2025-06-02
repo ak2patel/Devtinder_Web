3d96728c3500c6bf264890ab77539a24bbd89615
ak2patel <ankit.2201116cs@iiitbh.ac.in>
2025-06-02 14:23:50 +0530

    Feat: Show Connection Request

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Connections from "./components/Connections"
 import appStore from "./utils/appStore"
 import { Provider } from "react-redux"
 import Feed from "./components/Feed"
+import Requests from "./components/Requests"
 
 
 function App() {
@@ -20,7 +21,7 @@ function App() {
             <Route path="/login" element={<Login/>}/>
             <Route path="/profile" element={<Profile/>}/>
             <Route path="/connections" element={<Connections/>}/>
-            <Route path="/request" element={<Profile/>}/>
+            <Route path="/requests" element={<Requests/>}/>
           </Route>
          
         </Routes>
--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -30,15 +30,15 @@ const Connections = () => {
 
       {connections.map((connection)=> {
         
-        const {firstName,lastName,age,gender,photoUrl,about} = connection;
+        const {_id,firstName,lastName,age,gender,photoUrl,about} = connection;
         
         return ( 
-         <div className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
+         <div  key={_id} className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
             <div><img alt="photo" className="w-20 h-20 rounded-full"  src={photoUrl}/></div>
             <div className="text-left mx-4"> <h2 className="font-bold text-xl">{firstName + " "+lastName}</h2>
             {age&&gender&&<p>{age + " , "+ gender}</p>}
             <p>{about}</p></div>
-            
+           
            
          </div>
          )
